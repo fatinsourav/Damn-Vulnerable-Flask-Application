fd14d3940b1c9604618353bdeec8f2f178f396b2
fatinsourav <fatin151485@gmail.com>
2019-12-09 11:36:38 +0600

    bugs fixed

--- vulnerableEnvironment/app.py
+++ vulnerableEnvironment/app.py
@@ -8,12 +8,12 @@ def test():
 @app.route('/home')
 def home():
 	return render_template("home.html")
-@app.route('/login'):
+@app.route('/login')
 def login():
 	return render_template("login.html")
 @app.route('/signup')
 def signup():
-	return render_template('signup')	
+	return render_template('signup.html')	
 @app.route('/openRedirect')
 def openRedirect():
 	return render_template('openRedirect.html')
