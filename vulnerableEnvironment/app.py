a9a94e2bea3ab3430de1b2f60f19a24d7acd9bd0
fatinsourav <fatin151485@gmail.com>
2019-12-09 13:25:23 +0600

    bug fixed

--- vulnerableEnvironment/app.py
+++ vulnerableEnvironment/app.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template
 
 app = Flask (__name__)
 
-@app.route('/test')
-def test():
-	return "This is a test string"
+
 @app.route('/home')
 def home():
 	return render_template("home.html")
