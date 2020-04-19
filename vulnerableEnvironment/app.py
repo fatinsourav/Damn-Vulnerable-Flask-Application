8e6c93f043fb76bdeb946fca9de2d3254773f7bb
fatinsourav <fatin151485@gmail.com>
2020-04-19 13:55:47 +0600

    fix pushed

--- vulnerableEnvironment/app.py
+++ vulnerableEnvironment/app.py
@@ -13,6 +13,13 @@ from Crypto import Random
 from flask import Flask,redirect,request, render_template_string, render_template,session,flash,url_for,session,logging
 from flask_sqlalchemy import SQLAlchemy
 app = Flask (__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite://///Users/fatinsourav/Damn-Vulnerable-Flask-Application/vulnerableEnvironment/database.db'
+db = SQLAlchemy(app)
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(80))
 
 APP_NAME = 'Damn Vulnerable Flask Application'
 
@@ -27,7 +34,29 @@ def rp(command):
     return popen(command).read()
 
 
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        uname = request.form["uname"]
+        passw = request.form["passw"]
+        
+        login = user.query.filter_by(username=uname, password=passw).first()
+        if login is not None:
+            return redirect(url_for("index"))
+    return render_template("login.html")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        uname = request.form['uname']
+        mail = request.form['mail']
+        passw = request.form['passw']
 
+        register = user(username = uname, email = mail, password = passw)
+        db.session.add(register)
+        db.session.commit()
+
+        return redirect(url_for("login"))
+    return render_template("register.html")    
 @app.route('/dashboard')
 def index():
     return """
