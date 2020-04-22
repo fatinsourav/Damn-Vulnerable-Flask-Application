2ce522d5c5cdaf2dc481e60b1cf9c796032598f1
fatinsourav <fatin151485@gmail.com>
2020-04-22 19:54:03 +0600

    database fixed

---
+++ vulnerableEnvironment/database.py
@@ -0,0 +1,16 @@
+import sqlite3 as sql
+
+conn = sql.connect("test.db")
+cur = conn.cursor()
+
+# =================
+#  Query execution
+# =================
+query = ('''CREATE TABLE USERS2
+    (username TEXT NOT NULL,
+     email TEXT Primary key,
+     password TEXT NOT NULL,
+     contact  INT);''')
+cur.execute(query)
+
+conn.close()
